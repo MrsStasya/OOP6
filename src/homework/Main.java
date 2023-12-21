56cf03eef7e7a212622c6cb7daf4ea6b2d0480e7
Anastasia S <aparicion@mail.ru>
2023-12-21 11:57:39 +0300

    Ready

---
+++ src/homework/Main.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class Main{
+    public static void main(String[] args){
+        User user = new User("Bob");
+        Persister persister = new Persister();
+        persister.save(user);
+        UserReport report = new UserReport();
+        report.report(user);
+
+    }
+}
